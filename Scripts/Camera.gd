e9902dea5d9687b1c56b7f2b35e98452a120a5ff
James Fraser-Jones <jf14218@my.bristol.ac.uk>
2020-09-15 14:45:48 +0100

    camera workig

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -5,30 +5,31 @@ export var targetPath : NodePath
 export var lookSensitivity : float = 0.004
 export var initAngle : float = -45
 
+var camRotation : Vector2 = Vector2(0, deg2rad(initAngle))
 var target : Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	target = get_node(targetPath)
-	rotate_object_local(Vector3.RIGHT, deg2rad(initAngle))
-	updatePosition()
-
-func _input(event) -> void:
+	updateRotation()
+	
+func _input(event):
 	if event is InputEventMouseMotion:
-		look(event.relative)
+		camRotation.x = fposmod(camRotation.x - event.relative.x * lookSensitivity, deg2rad(360))
+		camRotation.y = clamp(fmod(camRotation.y - event.relative.y * lookSensitivity, deg2rad(360))
+			, deg2rad(-90), deg2rad(90))
+		updateRotation()
 
 func _process(delta: float) -> void:
 	updatePosition()
 
-func look(mouseMovement: Vector2) -> void:
-	var basis = transform.basis
-	rotate(Vector3.UP, -mouseMovement.x * lookSensitivity)
-	rotate_object_local(Vector3.RIGHT, -mouseMovement.y * lookSensitivity)
-	orthonormalize() #correct for floating point error accumulation
-	if rotation_degrees.z != 0: #clamp to hemi-circle (this needs to jump to max instead or something quarternions hmmm)
-		transform.basis = basis
+func updateRotation() -> void:
+	transform.basis = Basis()
+	rotate_object_local(Vector3.UP, camRotation.x)
+	rotate_object_local(Vector3.RIGHT, camRotation.y)
 
 func updatePosition() -> void:
 	translation = target.translation
 	translate_object_local(Vector3.BACK * radius)
+
 
