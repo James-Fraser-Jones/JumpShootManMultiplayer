08092c767d8ce1cb8dc7c4877d1a882dce20cf4e
James Fraser-Jones <jf14218@my.bristol.ac.uk>
2020-09-19 00:21:43 +0100

    camera smoothing and yoffset interpolation fully working

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -1,46 +1,67 @@
 extends Camera
 
-export var radius : float = 5
-export var verticalSensitivity : float = 0.004
-export var horizontalSensitivity : float = 0.004
+export var radius : float = 5 #radius at which camera orbits around target
 export var minPitch : float = -90
 export var maxPitch : float = 90
 export var initPitch : float = 45
-export var yOffset : float = 2
-export var margin : float = 0.2
+export var yOffset : float = 2 #how high camera hovers above target object
+export var margin : float = 0.1 #how far away camera stays from colliding objects
+export var cameraSmoothing : float = 0.4 #how many seconds required to "catch up"
+export var cameraSensitivity : float = 0.2 #multiplier of raw mouse co-ordinates
 export var targetPath : NodePath
 
-var camRotation : Vector2 = Vector2(0, deg2rad(-initPitch))
-var target : Spatial
+var camDebt : Vector2 = Vector2.ZERO #record of amount of rotation to apply in future (for camera smoothing)
+var camRotation : Vector2 = Vector2(0, deg2rad(-initPitch)) #orbit camera rotation in radians
+var target : Spatial #reference to target object
+var distance : float = 1 #how far away camera is from target due to clipping behavior
 
 func _ready() -> void:
 	target = get_node(targetPath)
 	
 func _input(event):
 	if event is InputEventMouseMotion:
-		camRotation.x = fposmod(camRotation.x - event.relative.x * horizontalSensitivity
-			, deg2rad(360))
-		camRotation.y = clamp(fmod(camRotation.y - event.relative.y * verticalSensitivity
-			, deg2rad(360))
-			, deg2rad(-maxPitch), deg2rad(-minPitch))
+		camDebt += event.relative * cameraSensitivity
 
 func _physics_process(delta):
-	#need to impose an artifical max rotational speed per physics frame
-	#include delta in order to more evenly distribute rotation across frames
+	#calculate debt to pay this frame
+	var xPaid : float = min_zero(camDebt.x * (delta / cameraSmoothing), camDebt.x)
+	var yPaid : float = min_zero(camDebt.y * (delta / cameraSmoothing), camDebt.y)
 	
-	#update rotation
+	#pay debt into camRotation
+	camDebt.x -= xPaid
+	camDebt.y -= yPaid
+	camRotation.x = fposmod(camRotation.x - deg2rad(xPaid), deg2rad(360))
+	camRotation.y = fmod(camRotation.y - deg2rad(yPaid), deg2rad(360))
+	
+	#clamp pitch
+	if (camRotation.y < -deg2rad(maxPitch)):
+		camRotation.y = -deg2rad(maxPitch)
+		camDebt.y = 0
+	elif (camRotation.y > -deg2rad(minPitch)):
+		camRotation.y = -deg2rad(minPitch)
+		camDebt.y = 0
+	
+	#update transform with new camRotation
 	transform.basis = Basis()
 	rotate_object_local(Vector3.UP, camRotation.x)
 	rotate_object_local(Vector3.RIGHT, camRotation.y)
 	
-	#set up positions
-	var orbitPosition : Vector3 = target.translation + Vector3.UP * yOffset
-	var camPosition : Vector3 = orbitPosition + transform.basis.z * radius
-	
 	#cast ray and check for collision
+	var camPosition : Vector3 = target.translation + transform.basis.z * radius
 	var space_state = get_world().direct_space_state
-	var result : Dictionary = space_state.intersect_ray(orbitPosition, camPosition, [target])
-	if result: camPosition = result.position + result.normal * margin
+	var result : Dictionary = space_state.intersect_ray(target.translation, camPosition, [target])
+	if result: 
+		distance = (result.position - target.translation).length() / (camPosition - target.translation).length()
+		camPosition = result.position + result.normal * margin
+	else: distance = 1
 	
-	#apply translation
+	#apply yOffset and translation
+	camPosition += Vector3.UP * yOffset * distance
 	translation = camPosition
+
+#return value closest to 0
+func min_zero(a: float, b: float) -> float:
+	if abs(a) <= abs(b):
+		return a
+	else:
+		return b
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,15 +2,15 @@ extends RigidBody
 
 export var runForce : float = 40000
 export var jumpForce : float = 300
-export var cameraPath : NodePath
 export var rotationSpeed : float = 10
-
-var camera : Camera
+export var cameraPath : NodePath
 
 #vars for linear interpolation of y-axis rotation as character moves
 var rotationWeight : float = 1
-var oldRotation : Quat
-var targetRotation : Quat
+var oldRotation : float
+var newRotation : float
+
+var camera : Camera
 
 func _ready() -> void:
 	camera = get_node(cameraPath)
@@ -29,21 +29,17 @@ func _physics_process(delta: float) -> void:
 func _process(delta: float) -> void:
 	if rotationWeight < 1:
 		rotationWeight = min(1, rotationWeight + rotationSpeed * delta)
-		$MeshInstance.transform.basis = Basis(oldRotation.slerp(targetRotation, rotationWeight))
+		$MeshInstance.rotation.y = lerp_angle(oldRotation, newRotation, rotationWeight)
 
 func _input(event):
 	if event.is_action_pressed("ui_select"):
 		apply_central_impulse(Vector3.UP * jumpForce)
 
 func handleRotation(movement: Vector2) -> void:
-	#NOTE: the first rotation "rotated(Vector3.RIGHT, deg2rad(-90))" is only necessary
-	#to counteract the fact that I have to rotate the MeshInstance by -90 in the x axis
-	#to get the correct orientation for the pill shape
-	var newRotation = Quat(Basis().rotated(Vector3.RIGHT, deg2rad(-90)).rotated(Vector3.UP,
-		camera.camRotation.x - movement.angle() - deg2rad(90)))
-	if newRotation != targetRotation:
-		oldRotation = Quat($MeshInstance.transform.basis)
-		targetRotation = newRotation
+	var rot = camera.camRotation.x - movement.angle() - deg2rad(90)
+	if rot != newRotation:
+		oldRotation = $MeshInstance.rotation.y
+		newRotation = rot
 		rotationWeight = 0
 
 #just found out about lerp_angle()
